b0a0847ce17140136cdc729408bfaa7c712cb23e
Simon Lidberg <simon.lidberg@gmail.com>
2016-12-29 12:13:46 +0100

    Completed the fourth mini-project and added *.png to gitignore.

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,21 +24,47 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+plt.draw()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import cross_val_score
+from time import time
 
+# k-nearest neighbours, classic, simple, easy to understand
+# sklearn.ensamble.AdaBoostClassifier. adaboost, ensamble methods
+# random forest, ensamble methods
+###
+# 1. Do some research.
+# 2. Find sklearn documentation.
+# 3. Deploy it!
+# 4. Use it to make predictions.
+# 5. Evaluate it using accuracy_score.
+###
+print "Evaluating using AdaBoost"
 
+### Create the classifier with the chosen algorithm.
+clf = AdaBoostClassifier(n_estimators=100, learning_rate=1.0)
 
+### Fit the training data.
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
 
+### Predict on the test data.
+t0 = time()
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t0, 3), "s"
 
-
-
+### Print accuracy.
+print accuracy_score(labels_test, pred)
 
 try:
     prettyPicture(clf, features_test, labels_test)
+    plt.show()
 except NameError:
     pass
