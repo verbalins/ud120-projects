5c0bf1a97470fd00b0458c6eafb647aef43bfc50
Simon Lidberg <simon.lidberg@gmail.com>
2016-12-29 10:24:41 +0100

    Completed SVM mini-project.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,6 +24,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
+print "Classifying with Naive Bayes"
+
 clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,7 +10,6 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
@@ -24,7 +23,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+print "Classifying with SVM"
+
+### Create classifier
+clf = SVC(C=10000.0, kernel="rbf")
+
+### Slice the training data down to 1%.
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100]
+
+### Fit the training data.
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+### Predict on the test data.
+t0 = time()
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t0, 3), "s"
+
+### Print accuracy.
+print accuracy_score(labels_test, pred)
+print "Element 10: ", pred[10]
+print "Element 26: ", pred[26]
+print "Element 50: ", pred[50]
+nrChris = pred.sum()
+print "Number of predicted mails written by Chris: ", nrChris
+print "Number of predicted mails written by Sara: ", len(pred)-nrChris
 #########################################################
-
-
